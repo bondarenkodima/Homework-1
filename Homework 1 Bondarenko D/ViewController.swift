01792b66faed586dc23472295353d65c5fe33b04
Dmitry <bondarenkodimay@gmail.com>
2022-11-06 11:36:34 +0200

    Initial Commit

---
+++ Homework 1 Bondarenko D/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 1 Bondarenko D
+//
+//  Created by MacBook Pro on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
