2650b7c05f614b71f695f7b243e3181b9f526963
Dmitry <bondarenkodimay@gmail.com>
2022-11-10 11:15:52 +0200

    init comit

--- Homework 1 Bondarenko D/ViewController.swift
+++ Homework 1 Bondarenko D/ViewController.swift
@@ -11,9 +11,151 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        workingWithArray()
+        arrayMonth()
+        replacementValues()
+        createYears()
+        comparisonValue()
+        searchWord()
+        if let message = batteryPercentage(charge: 75) {
+            print(message)
+        }
+    }
+    
+    func workingWithArray() {
+        var arrayOne = [-21, -13, -7, -5, -1, 2, 6, 9, 77, 99]
+        
+        // 1. Замена отрицательных чисел
+        
+        for (index, element) in arrayOne.enumerated() {
+            if element < 0 {
+                arrayOne[index] = 0
+            }
+        }
+        print(arrayOne)
+        
+        // 2. Вывод min и max
+//        var min = 0
+//        var max = 0
+//
+//        for number in arrayOne {
+//            if number < min {
+//                min = number
+//            } else if max < number {
+//                max = number
+//            }
+//        }
+        
+        arrayOne.sort { numLeft, numRight in
+          return numLeft < numRight
+        }
+        print(arrayOne.first ?? 0)
+        print(arrayOne.last ?? 0)
+        
+//        print("Минимально число в массиве = \(min)")
+//        print("Максимальное число в массиве = \(max)")
+        
+        // 3. Вывод sum
+        var sum = 0
+        
+        for number in arrayOne {
+            sum += number
+        }
+        print("Сумма чисел в массиве = \(sum)")
+    }
+    
+    // 4. !!!!!!!!!!!!!!!!!!!!!!!!!
+    func arrayMonth() {
+        var monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        
+        var monthsDictionary = [Int: String]()
+        
+        for (index, element) in monthsArray.enumerated() {
+            monthsDictionary[index] = element
+        }
+        print(monthsDictionary)
+    }
+    
+    // 5. Смена значений по ключам
+    func replacementValues() {
+        var dictionary = ["first": 1, "second": 2, "third": 3, "fourth": 4]
+        var temp1 = dictionary["first"]
+        var temp2 = dictionary["fourth"]
+        
+        dictionary["first"] = temp2
+        dictionary["fourth"] = temp1
+        print(dictionary)
+    }
+    
+    // 6.
+    func createYears() {
+        var calendar: [Int: [String]] = [:]
+        var arrayMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        
+        for element in 1970...2022 {
+            calendar[element] = arrayMonth
+        }
+        print(calendar)
+        
+        // 7.
+        for (key, value) in calendar {
+            var newMonth = value
+            newMonth.append("xxxxxx")
+            calendar[key] = newMonth
+        }
+        print(calendar)
+        
+        // 8.
+        
+        
+
+
     }
 
+    
 
+    
+    
+    // 9. Сравнение значений
+    func comparisonValue() {
+        let firstValue = 7
+        let secondValue = 13
+        let sum = firstValue + secondValue
+        
+        if firstValue == secondValue {
+            //sumResult = sum * 3
+            print(sum * 3)
+        } else if sum % 2 == 0 {
+            print(sum)
+        }
+    }
+    
+    // 10. Достать слово !!!!!!!!!!!!!!!!!!!!!!!!!!
+    func searchWord() {
+        let sentence = "My first home work!"
+        let sentence2 = sentence.replacingOccurrences(of: "!", with: "")
+        let strArray = sentence2.components(separatedBy:  " ")
+        print((strArray.first ?? "") + " " + (strArray.last ?? ""))
+        print(strArray.last)
+    }
+    
+    // 11. Switch !!!!!!!!!!!!!!! return message !!!!!!!!!!!!!!!!!!!
+    func batteryPercentage(charge: Int) -> String? {
+  
+        switch charge {
+        case 100:
+            return "Устройство заряжено"
+        case 70...80:
+            return "Необходимо зарядить устройство в течении 6 часов"
+        case 20...40:
+            return "Поставьте устройство на зарядку"
+        case 0:
+            return "Устройство полностью разряжено"
+
+        default:
+            return nil
+        }
+    }
 }
 
